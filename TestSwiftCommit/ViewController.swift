2970034eb82bbf84d92197f94f874c497451cb7a
kmorrical <kathryn.morrical@gmail.com>
2017-01-17 09:04:46 -0800

    second commit

--- TestSwiftCommit/ViewController.swift
+++ TestSwiftCommit/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let userName = "dummyUser"
+    let userName = "dumm1User"
     
     override func viewDidLoad() {
         super.viewDidLoad()
